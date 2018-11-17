0996b1e0ccb4c67b1477b6ee07a3a1ca499baaf1
Navid Anindya <navidanindya@hotmail.com>
2018-11-18 01:57:03 +0600

    Some changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,20 +9,66 @@
             <div class="col">
                 <h2>I'm <i>Navid Anindya.</i></h2>
                 <p>
-                    Currently making this site.
+                    You'll see my blog here soon.
                 </p>
                 <p>
-                    For now please check my <a href="https://github.com/nealtheguitarist" target="_blank">GitHub</a>
-                    and <a href="https://www.linkedin.com/in/navidanindya" target="_blank">LinkedIn</a>.
+                    Until then, please check my:
+                    <ul class="no-bullets">
+                        <li v-for="(link, key) in links" :key="key">
+                            <a :href="link.link" target="_blank" rel="noopener">{{ link.name }}</a>
+                        </li>
+                    </ul>
                 </p>
-                <h5>Have a great day! :D</h5>
+                <h5>Have a <i>GREAT</i> day! :D</h5>
             </div>
         </div>
     </div>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    name: 'GitHub',
+                    link: 'https://github.com/nealtheguitarist'
+                },
+                {
+                    name: 'LinkedIn',
+                    link: 'https://www.linkedin.com/in/navidanindya'
+                },
+                {
+                    name: 'StackOverflow',
+                    link: 'https://stackoverflow.com/story/nealtheguitarist'
+                }
+            ]
+        };
+    }
+}
+</script>
+
+
 <style scoped>
 .text-center {
     text-align: center;
+}
+
+.no-bullets {
+    list-style-type: none;
+}
+
+a {
+    color: #2C71A5;
+    font-weight: bold;
+    text-decoration: none;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+
+a:visited {
+    color: #2C71A5;
 }
 </style>
