028beb751eaa65274116e629aa11abf70041c6ae
Navid Anindya <navidanindya@gmail.com>
2021-05-11 16:44:38 +0600

    WIP: disable firebase. work on it later.

--- nuxt.config.js
+++ nuxt.config.js
@@ -99,26 +99,26 @@ export default {
   },
 
   // Firebase Cloud Firestore
-  firebase: {
-    config: {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID,
-      measurementId: process.env.FIREBASE_MEASUREMENT_ID
-    },
-    lazy: true,
-    services: {
-      firestore: {
-        enablePersistence: {
-          synchronizeTabs: true
-        }
-      }
-    },
-    terminateDatabasesAfterGenerate: true
-  },
+  // firebase: {
+  //   config: {
+  //     apiKey: process.env.FIREBASE_API_KEY,
+  //     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  //     projectId: process.env.FIREBASE_PROJECT_ID,
+  //     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  //     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  //     appId: process.env.FIREBASE_APP_ID,
+  //     measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  //   },
+  //   lazy: true,
+  //   services: {
+  //     firestore: {
+  //       enablePersistence: {
+  //         synchronizeTabs: true
+  //       }
+  //     }
+  //   },
+  //   terminateDatabasesAfterGenerate: true
+  // },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
