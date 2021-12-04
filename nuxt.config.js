11a720c9a072586505b84283fd7169e4ddc34846
Navid Anindya <navidanindya@gmail.com>
2021-12-05 03:24:31 +0600

    Tiny fix in 404 config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  fallback: true,
+  generate: {
+    fallback: true
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
