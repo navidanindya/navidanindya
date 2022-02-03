7e8a810fb22ff7ee23ec4bc89f229e0d32aa1613
Navid Anindya <navidanindya@gmail.com>
2022-02-04 05:52:46 +0600

    Update nuxt/content and nuxt/tailwindcss, remove extra eslint packages.

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,8 +48,6 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     // https://google-fonts.nuxtjs.org/
